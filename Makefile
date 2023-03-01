4415d1f5b86e00115e264906f16c4e081648591c
Steven Potts <sdpotts93@gmail.com>
2023-03-01 15:08:24 -0600

    twenty five inputs fields

--- Makefile
+++ Makefile
@@ -1,2 +1,2 @@
 deploy:
-	aws s3 cp . s3://custom-wf-code/ --recursive --metadata-directive REPLACE --cache-control max-age=86400 --exclude 'ssh:/*' --exclude '.gitignore' --exclude '.git/*' --profile prmg2
+	aws s3 cp . s3://custom-wf-code/twenty-five/ --recursive --metadata-directive REPLACE --cache-control max-age=86400 --exclude 'ssh:/*' --exclude '.gitignore' --exclude '.git/*' --profile prmg2
--- contact/js/contact.js
+++ contact/js/contact.js
@@ -2,6 +2,9 @@
 
 const calendlyWidgetButton = document.querySelector(".button.cta-white.w-button");
 
+// All text fields
+const inputTextFields = document.querySelectorAll(".bp-contact-field");
+
 // ----------------- LISTENERS ----------------------
 
 window.addEventListener('message', checkCalendlyEventScheduled, false);
@@ -13,6 +16,28 @@ calendlyWidgetButton.addEventListener("click", (e) => {
   openCalendlyWidget();
 
   return false;
+});
+
+// Change all text fields styles on focus and on blur
+
+inputTextFields.forEach(item => {
+
+  item.addEventListener("focus", () => {
+    const label = item.parentElement.querySelector(".form-label");
+    label.style.transform = 'translateY(-10px)';
+    label.style.opacity = "1";
+  });
+
+  item.addEventListener("blur", () => {
+
+    const label = item.parentElement.querySelector(".form-label");
+
+    if (!item.value) {
+      label.style.transform = 'translateY(0px)';
+      label.style.opacity = "0";
+    }
+  });
+
 });
 
 // ----------------- FUNCTIONS ----------------------
