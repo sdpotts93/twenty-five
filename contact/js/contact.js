5b887def75d2d4adfe2c4e6cd153a0e8d42cfee9
Steven Potts <stevenpotts@192.168.1.15>
2023-11-15 22:19:54 -0600

    beacon

--- contact/js/contact.js
+++ contact/js/contact.js
@@ -1,20 +1,9 @@
 // ----------------- VARIABLES ----------------------
 
-const calendlyWidgetButton = document.querySelector(".button.cta-white.w-button");
-
 // All text fields
 const inputTextFields = document.querySelectorAll(".bp-contact-field");
 
 // ----------------- LISTENERS ----------------------
-
-calendlyWidgetButton.addEventListener("click", (e) => {
-  e.preventDefault();
-  e.stopPropagation();
-
-  overlay.style.display = 'block';
-
-  return false;
-});
 
 // Change all text fields styles on focus and on blur
 
@@ -38,4 +27,43 @@ inputTextFields.forEach(item => {
 
 });
 
+document.querySelector(".footer-help").addEventListener("click", () => {
+  if (!window.Beacon) {
+    initHelpscoutFooter();
+  } else {
+    window.Beacon('open');
+  }
+})
 // ----------------- FUNCTIONS ----------------------
+
+function initHelpscoutFooter () {
+
+  if (window.trackingHelpscoutDidInit) {
+    return false;
+  }
+
+  // flag to ensure script does not get added to DOM more than once.
+  window.trackingHelpscoutDidInit = true;
+
+  !function(e, t, n) {
+    function a() {
+      var e = t.getElementsByTagName("script")[0],
+        n = t.createElement("script");
+      n.type = "text/javascript", n.async = !0, n.src = "https://beacon-v2.helpscout.net", e.parentNode.insertBefore(n, e)
+    }
+    if (e.Beacon = n = function(t, n, a) {
+        e.Beacon.readyQueue.push({
+          method: t,
+          options: n,
+          data: a
+        })
+      }, n.readyQueue = [], "complete" === t.readyState) return a();
+    e.attachEvent ? e.attachEvent("onload", a) : e.addEventListener("load", a, !1)
+  }(window, document, window.Beacon || function() {});
+
+  window.Beacon('init', 'ba7a8fe2-8a9d-4a7b-ae56-770b3a9c6584');
+  window.Beacon('on', 'ready', () => {
+    window.Beacon('open');
+  });
+
+}
--- homepage/js/homepage.js
+++ homepage/js/homepage.js
@@ -1,10 +1,44 @@
 // ----------------- VARIABLES ----------------------
 
-const beaconContainer = document.querySelector("#beacon-container");
-
 // ----------------- LISTENERS ----------------------
 
 document.querySelector(".footer-help").addEventListener("click", () => {
-  beaconContainer.click();
+  if (!window.Beacon) {
+    initHelpscoutFooter();
+  } else {
+    window.Beacon('open');
+  }
 })
 // ----------------- FUNCTIONS ----------------------
+
+function initHelpscoutFooter () {
+
+  if (window.trackingHelpscoutDidInit) {
+    return false;
+  }
+
+  // flag to ensure script does not get added to DOM more than once.
+  window.trackingHelpscoutDidInit = true;
+
+  !function(e, t, n) {
+    function a() {
+      var e = t.getElementsByTagName("script")[0],
+        n = t.createElement("script");
+      n.type = "text/javascript", n.async = !0, n.src = "https://beacon-v2.helpscout.net", e.parentNode.insertBefore(n, e)
+    }
+    if (e.Beacon = n = function(t, n, a) {
+        e.Beacon.readyQueue.push({
+          method: t,
+          options: n,
+          data: a
+        })
+      }, n.readyQueue = [], "complete" === t.readyState) return a();
+    e.attachEvent ? e.attachEvent("onload", a) : e.addEventListener("load", a, !1)
+  }(window, document, window.Beacon || function() {});
+
+  window.Beacon('init', 'ba7a8fe2-8a9d-4a7b-ae56-770b3a9c6584');
+  window.Beacon('on', 'ready', () => {
+    window.Beacon('open');
+  });
+
+}
